0eaca375cec4af4d9ed6d968c362c6a422850cdc
Luiz Felipe <aguiarpassaroni@gmail.com>
2022-06-29 21:28:07 -0400

    salve

--- GameCode.swift
+++ GameCode.swift
@@ -1,5 +1,5 @@
 import Foundation
-let circle = OvalShape(width: 150, height: 150)
+let ball = OvalShape(width: 40, height: 40)
 let barrierWidth = 300.0
 let barrierHeight = 25.0
 
@@ -19,6 +19,19 @@ let funnelPoints = [
 ]
 let funnel = PolygonShape(points: funnelPoints)
 
+   let targetPoints = [
+    Point(x: 10, y: 0),
+    Point(x: 0, y: 10),
+    Point(x: 10, y: 20),
+    Point(x: 20, y: 10)
+]
+
+let target = PolygonShape(points:
+                            targetPoints)
+
+
+
+
 /*
 The setup() function is called once when the app launches. Without it, your app won't compile.
 Use it to set up and start your app.
@@ -29,26 +42,60 @@ for example if statements and for loops, at the top level; they have to be writt
 of a function.
 */
 
-func setup() {
-    circle.position = Point(x:200, y: 400)
-    scene.add(circle)
-    circle.hasPhysics = true
-    circle.fillColor = .blue
-    
+fileprivate func setupBall() {
+    ball.position = Point(x:250, y: 400)
+    scene.add(ball)
+    ball.hasPhysics = true
+    ball.fillColor = .blue
+}
+
+fileprivate func setupBarrier() {
     barrier.position = Point(x:200, y: 150)
     barrier.hasPhysics = true
     scene.add(barrier)
     barrier.isImmobile = true
-    
+    barrier.fillColor = .brown
+}
+
+fileprivate func setupFunnel() {
     funnel.position = Point(x:200, y: scene.height - 25)
     scene.add(funnel)
     funnel.onTapped = dropBall
+    funnel.fillColor = .gray
+}
+
+func setup() {
+    setupBall()
+
+    
+    setupBarrier()
+    
+    setupFunnel()
+    
 }
 func dropBall(){
-    circle.position = funnel.position
+    ball.position = funnel.position
     
     
 }
+func setupTarget() {
+    target .position = Point(x:200, y:400)
+    target .hasPhysics = true
+    target .isImpermeable = false
+    target .fillColor = .yellow
+    target.name = "target"
+    
+    scene.add(target)
 
+}
 
+func ballCollided(with otherShape: Shape) {
+    otherShape.fillColor = .green
+    ball.onCollision = ballCollided(with:)
+}
 
+func ballCollided(with otherShape: Shape) {
+    if otherShape.name != "target" { return }
+    
+    otherShape.fillColor = .green
+}
